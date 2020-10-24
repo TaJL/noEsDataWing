9c0f3f1122042076f52f8da83bcaa7628509092c
Tadeo <tadeo@ravegan.com>
2020-10-24 00:11:13 -0300

    controls fixed

--- Assets/ControlManager.cs
+++ Assets/ControlManager.cs
@@ -1,14 +1,53 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class ControlManager : MonoBehaviour
 {
+    private Action OnActionPressedEvent;
+
+    private PlayerInput _playerInput = null;
+
+    private InputAction _directionAction = null;
+    private InputAction _actionAction = null;
+
+    private const string DIRECTION = "Direction";
+    private const string ACTION = "Action";
+
     private void Awake()
     {
-        _playerInput.currentActionMap = gameInputs.Dorf;
+        _playerInput = GetComponent<PlayerInput>();
+        Controls gameInputs = new Controls();
+        _playerInput.currentActionMap = gameInputs.Game;
         _playerInput.actions.Enable();
         _directionAction = _playerInput.actions[DIRECTION];
-        _jumpAction = _playerInput.actions[JUMP];
+        _actionAction = _playerInput.actions[ACTION];
+    }
+
+    private void OnEnable()
+    {
+        if (_playerInput == null)
+        {
+            return;
+        }
+
+        _actionAction.started += context => OnActionPressedEvent?.Invoke();
+    }
+
+    private void OnDisable()
+    {
+        if (_playerInput == null)
+        {
+            return;
+        }
+
+        _actionAction.started -= context => OnActionPressedEvent?.Invoke();
+    }
+
+    public Vector2 GetDirection()
+    {
+        return _directionAction.ReadValue<Vector2>();
     }
 }
