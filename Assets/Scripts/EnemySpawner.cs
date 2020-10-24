66c057c20a6d1bb1a3b6da249d249e1d70c4be6e
Faulieldin <52634804+Faulieldin@users.noreply.github.com>
2020-10-23 22:35:41 -0300

    PoolEnemies

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    
+    [SerializeField]private int CantidadEnemigos;
+    [SerializeField]private float VelocidadEnemigos;
+    [SerializeField]private float MinXSpawn;
+    [SerializeField]private float MaxXSpawn;
+    [SerializeField]private float MinYSpawn;
+    [SerializeField]private float MaxYSpawn;
+    
+    [SerializeField]private List<GameObject> Enemies;
+    private Vector2 SpawnPosition;
+    private Vector2 RandomPositionSpawn;
+
+    private void Awake()
+    {
+        SpawnPosition=transform.position;
+
+        if(CantidadEnemigos==0){
+            CantidadEnemigos=5;
+        }
+
+        if(VelocidadEnemigos==0){
+            VelocidadEnemigos=2;
+        }
+
+        
+    }
+    private void Start()
+    {
+        for(int i=0;i<CantidadEnemigos;i++){
+            GameObject aux = Instantiate(Resources.Load("Enemy")as GameObject,SpawnPosition,Quaternion.identity);
+            Enemies.Add(aux);
+            aux.SetActive(false);
+        }
+    }
+}
