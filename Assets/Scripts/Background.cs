83739ad9b89bd1a8b7f2bfccfb35ad63dafc520b
Tadeo <tadeo@ravegan.com>
2020-10-25 01:50:10 -0300

    background looping

---
+++ Assets/Scripts/Background.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour
+{
+    private float _textureUnitSize = 0;
+    private Transform _target = null;
+
+    private void Awake()
+    {
+        Sprite sprite = GetComponent<SpriteRenderer>().sprite;
+        Texture2D texture = sprite.texture;
+        _textureUnitSize = texture.height / sprite.pixelsPerUnit;
+    }
+
+    private void OnEnable()
+    {
+        ShipLocomotion.OnInstantiatedEvent += UpdateTarget;
+    }
+
+    private void OnDisable()
+    {
+        ShipLocomotion.OnInstantiatedEvent -= UpdateTarget;
+    }
+
+    private void UpdateTarget(ShipLocomotion ship)
+    {
+        _target = ship.transform;
+        Manager.IsNull(_target);
+    }
+
+    private void LateUpdate()
+    {
+        if (_target.position.y - transform.position.y >= _textureUnitSize)
+        {
+            transform.position += new Vector3(0, _textureUnitSize, 0);
+        }
+    }
+}
