f10a14955c6dd2aa671cc9228285bded4f361b3d
Faulieldin <52634804+Faulieldin@users.noreply.github.com>
2020-10-25 19:41:44 -0300

    Pseudo Menu

---
+++ Assets/Scripts/Menu/MenuManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+
+public class MenuManager : MonoBehaviour
+{
+    [SerializeField]private GameObject ScorePanel=null;
+    [SerializeField]private Text ScoreData=null;
+    public void OnButton(Text other){
+        other.fontSize += 2;
+    }
+
+    public void OffButton(Text other){
+        other.fontSize -= 2;
+    }
+
+    public void Play(){
+        SceneManager.LoadScene(1);
+    }
+    public void ScoreEnter(){
+        ScoreData.text = ("Score: " + DataSaver.Load().Score + " " + "Time: " + DataSaver.Load().TimeElapsed + " seconds");
+        ScorePanel.SetActive(true);
+    }
+    public void ScoreBack(){
+        ScorePanel.SetActive(false);
+    }
+}
